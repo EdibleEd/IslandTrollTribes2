6ee183277a02e97e1978dc3a9e37e8cad19fa896
dli42 <davelid136@gmail.com>
2014-08-30 01:19:53 -0400

    MixHerbs completed in the jankiest fucking broken style I'm ashamed

--- scripts/vscripts/custom_functions_ability.lua
+++ scripts/vscripts/custom_functions_ability.lua
@@ -47,6 +47,7 @@ function MixHerbs(keys)
     local caster = keys.caster
     --Table to identify ingredients
     local herbTable = {"item_river_stem", "item_river_root", "item_herb_butsu", "item_herb_orange", "item_herb_purple", "item_herb_yellow", "item_herb_blue"}
+    local specialTable = {"item_herb_orange", "item_herb_purple", "item_herb_yellow", "item_herb_blue"}
     --Table used to look up herb recipes, can move this if other functions need it
     local recipeTable = {
         {"item_spirit_wind", {item_river_stem = 2}},
@@ -62,20 +63,22 @@ function MixHerbs(keys)
         {"item_potion_manaiv", {item_river_stem = 3, item_herb_butsu = 3}},
         {"item_rock_dark", {item_river_root = 2, item_river_stem = 2, item_herb_butsu = 2}},
         {"item_potion_twin_island", {item_herb_orange = 3, item_herb_purple = 3}},
-        {"item_potion_twin_island", {item_herb_yellow = 3, item_herb_blue = 3}}
+        {"item_potion_twin_island", {item_herb_yellow = 3, item_herb_blue = 3}},
+        {"item_essence_bees", {item_herb_orange = 1, item_herb_purple = 1, item_herb_yellow = 1, item_herb_blue = 1}},
+        {"item_gem_of_knowledge", {item_herb_blue = 1, item_herb_orange = 3, item_herb_yellow}},
+        {"item_gem_of_knowledge", {item_herb_blue = 1, item_herb_orange = 3, item_herb_purple}},
+        {"item_potion_anti_magic", {special_1 = 6}},
+        {"item_potion_fervor", {special_1 = 3, item_herb_butsu = 1}},
+        {"item_potion_elemental", {special_1 = 1, item_river_stem = 3, item_river_root = 1}},
+        {"item_potion_disease", {special_1 = 2,special_2 = 2, item_river_root = 1}},
+        {"item_potion_nether", {special_1 = 1, item_river_stem = 2, item_herb_butsu = 2}},
+        {"item_essence_bees", {special_1 = 2, special_2 = 1, special__3 = 1}},
+        {"item_potion_acid", {special_1 = 2, special_2 = 2, item_river_stem = 2}}
     }
     
     --recipes that use special herbs. A bit more complicated
     --[[
-    {"item_potion_anti_magic", {special_1 = 6}},
-    {"item_potion_fervor", {special_1 = 3, item_herb_butsu = 1}},
-    {"item_potion_elemental", {special_1 = 1, item_river_stem = 3, item_river_root = 1}  },
-    {"item_potion_disease", {special_1 = 2,special_2 = 2, item_river_root = 1}},
-    {"item_potion_nether", {special_1 = 1, item_river_stem = 2, item_herb_butsu = 2}},
-    {"item_gem_of_knowledge", {item_herb_blue = 1, item_herb_orange = 3, yellow or purple}},
-    {"item_essence_bees", {item_herb_orange = 1, item_herb_purple = 1, item_herb_yellow = 1, item_herb_blue = 1}}, --or special_1 = 2, special_2, special_3
-    
-    {"item_potion_acid", {special_1 = 2, special_2 = 2, item_river_stem = 2}},
+
     --]]
     
     local myMaterials = {}
@@ -84,7 +87,7 @@ function MixHerbs(keys)
     --loop through inventory slots
     for i = 0,5 do
         local item = caster:GetItemInSlot(i)    --get the item in the slot
-        if item ~= nil then --if the slot is full
+        if item ~= nil then --if the slot is not empty
             local itemName = item:GetName() --get the item's name
             print(i, itemName)  --debug
             --loop through list of possible ingredients to see if the inventory item is one
@@ -96,7 +99,7 @@ function MixHerbs(keys)
                     end
                     myMaterials[itemName] = myMaterials[itemName] + 1   --increment the count
                     table.insert(itemTable, item)
-                end
+                end                
             end
         else
             print(i, "empty")  --more debug, print empty slot
@@ -115,8 +118,70 @@ function MixHerbs(keys)
                 caster:RemoveItem(removeMe)
             end
             caster:AddItem(newItem) --add the new item
+            return  --end the function, only one item per mix
+        end
+    end   
+    
+    
+    print("Check for special match")
+    local specialTable = {
+        {"item_herb_orange", 0},
+        {"item_herb_purple", 0},
+        {"item_herb_yellow", 0},
+        {"item_herb_blue", 0}
+    }
+    
+        specialTable[1][2] = myMaterials["item_herb_orange"]
+        specialTable[2][2] = myMaterials["item_herb_purple"]
+        specialTable[3][2] = myMaterials["item_herb_yellow"]
+        specialTable[4][2] = myMaterials["item_herb_blue"]
+    
+    for key,val in pairs (specialTable) do
+        print(val[1], val[2])
+        if val[2] == nil then
+            specialTable[key][2] = 0
         end
     end
+    
+    print("sort it!")            
+    table.sort(specialTable, compareHelper)
+    
+    for key,val in pairs (specialTable) do
+        print(val[1], val[2])
+    end
+    
+    --replace herb names with special_X
+    myMaterials["special_1"] = specialTable[1][2]
+    myMaterials[specialTable[1][1]] = nil
+    myMaterials["special_2"] = specialTable[2][2]
+    myMaterials[specialTable[2][1]] = nil
+    myMaterials["special_3"] = specialTable[3][2]
+    myMaterials[specialTable[3][1]] = nil
+    myMaterials["special_4"] = specialTable[4][2]
+    myMaterials[specialTable[4][1]] = nil
+    
+    for key,val in pairs (myMaterials) do
+        if val == 0 then
+            myMaterials[key] = nil
+        end
+    end
+    
+    print("Check for match")
+    --check if player materials matches any recipes
+    for i,value in pairs(recipeTable) do  --loop through the recipe table
+        local recipeName = recipeTable[i][1]    --get the name of the recipe
+        local recipeIngredients = recipeTable[i][2] --get the items needed for the recipe
+        if CompareTables(recipeIngredients, myMaterials) then    --if a recipe matches
+            print("Match!", i)
+            local newItem = CreateItem(recipeName, nil, nil)   --create the resulting item
+            for i,removeMe in pairs(itemTable) do   --delete the materials
+                caster:RemoveItem(removeMe)
+            end
+            caster:AddItem(newItem) --add the new item
+            return  --end the function, only one item per mix
+        end
+    end   
+    
 end
 
 --Compares two tables to see if they have the same values
@@ -148,4 +213,8 @@ function CompareTables(table1, table2)
     
     print("Match!")
     return true
+end
+
+function compareHelper(a,b)
+    return a[2] > b[2]
 end
