274099571b89c4f49a520253faf861418de6cc3a
dli42 <davelid136@gmail.com>
2014-09-01 15:46:53 -0400

    Updated Radar_Manipulations KV, calls swap script Added swap functions to custom abilities lua

--- scripts/vscripts/custom_functions_ability.lua
+++ scripts/vscripts/custom_functions_ability.lua
@@ -218,3 +218,66 @@ end
 function compareHelper(a,b)
     return a[2] > b[2]
 end
+
+function SwapAbilities(unit, ability1, ability2, enable1, enable2)
+    
+    --swaps ability1 and ability2, disables 1 and enables 2
+    print("swap", ability1:GetName(), ability2:GetName() )
+    unit:SwapAbilities(ability1:GetName(), ability2:GetName(), enable1, enable2)
+    ability1:SetHidden(enable2)
+    ability2:SetHidden(enable1)
+end
+
+function RadarManipulations(keys)
+    local caster = keys.caster
+    local isOpening = (keys.isOpening == "true")
+    local ABILITY_radarManipulations = caster:FindAbilityByName("ability_gatherer_radarmanipulations")
+    
+    local abilityLevel = ABILITY_radarManipulations:GetLevel()
+    print("abilityLevel", abilityLevel)
+    local unitName = caster:GetUnitName()
+    print(unitName)
+    
+    local tableDefaultSkillBook ={
+        "ability_gatherer_itemradar",
+        "ability_gatherer_radarmanipulations", 
+        "ability_empty3", 
+        "ability_empty4",  
+        "ability_empty5", 
+        "ability_empty6", 
+        "ability_empty7"}
+        
+    local tableRadarBook ={
+        "ability_gatherer_findmushroomstickortinder",
+        "ability_gatherer_findhide",
+        "ability_gatherer_findclayballcookedmeatorbone",
+        "ability_gatherer_findmanacrystalorstone",
+        "ability_gatherer_findflint",
+        "ability_gatherer_findmagic"
+    }
+
+    local numAbilities = abilityLevel
+
+    for i=1,numAbilities do
+        print(tableDefaultSkillBook[i], tableRadarBook[i])
+        local ability1 = caster:FindAbilityByName(tableDefaultSkillBook[i])
+        local ability2 = caster:FindAbilityByName(tableRadarBook[i])
+        if ability2:GetLevel() == 0 then
+            ability2:SetLevel(1)
+        end
+        print("isopening",isOpening)
+        if isOpening == true then
+            print("ability1:", ability1:GetName(), "ability2:", ability2:GetName())
+            SwapAbilities(caster, ability1, ability2, false, true)
+            caster:FindAbilityByName("ability_gatherer_radarmanipulations"):SetHidden(true)
+        else
+            SwapAbilities(caster, ability1, ability2, true, false)
+            caster:FindAbilityByName("ability_gatherer_radarmanipulations"):SetHidden(false)
+            caster:FindAbilityByName("ability_empty3"):SetHidden(true)
+            caster:FindAbilityByName("ability_empty4"):SetHidden(true)
+            caster:FindAbilityByName("ability_empty5"):SetHidden(true)
+            caster:FindAbilityByName("ability_empty6"):SetHidden(true)
+        end
+    end
+
+end
