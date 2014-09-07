d94000bf0ca11e69bbac7cb835f4cf7e817c7724
dli42 <davelid136@gmail.com>
2014-09-07 17:02:34 -0400

    Added precaching for some item models so they don't spawn as ERROR We need to find a better way to do this, precaching every model individually isn't practical

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -45,6 +45,11 @@ function Precache( context )
 	PrecacheResource("model", "models/items/wards/nexon_sotdaeward/nexon_sotdaeward.vmdl",context)
 	PrecacheResource("model", "models/items/furion/staff_eagle_1.vmdl",context)
 	PrecacheResource("particle","particles/dire_fx/fire_barracks_glow_b.vpcf",context)
+    
+    PrecacheResource("model", 'models/props_destruction/lion_groundspikes.vmdl',context)
+    PrecacheResource("model", 'models/items/abaddon/alliance_abba_weapon/alliance_abba_weapon_fx.vmdl',context)
+    PrecacheResource("model", 'models/particle/tiny_simrocks.vmdl',context)
+    PrecacheResource("model", 'models/particle/ice_shards.vmdl',context)
 	
     print("Precache Finish")
 end
