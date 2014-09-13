cb270f98df46c75a26d811fccac9928d4bb3f457
dli42 <davelid136@gmail.com>
2014-09-13 16:55:41 -0400

    Added a random positioning to items dropped on death

--- scripts/vscripts/custom_functions_item.lua
+++ scripts/vscripts/custom_functions_item.lua
@@ -26,10 +26,14 @@ function DropItemOnDeath(keys) -- keys is the information sent by the ability
                 		local Item = killedUnit:GetItemInSlot( itemSlot ) -- uses a variable which gets the actual item in the slot specified starting at 0, 1st slot, and ending at 5,the 6th slot.
                 		if Item ~= nil and Item:GetName() == itemName then -- makes sure that the item exists and making sure it is the correct item
                 			local newItem = CreateItem(itemName, nil, nil) -- creates a new variable which recreates the item we want to drop and then sets it to have no owner
-                    			CreateItemOnPositionSync(killedUnit:GetOrigin(), newItem) -- takes the newItem variable and creates the physical item at the killed unit's location
+                    			CreateItemOnPositionSync(killedUnit:GetOrigin() + RandomVector(RandomInt(20,100)), newItem) -- takes the newItem variable and creates the physical item at the killed unit's location
                     			killedUnit:RemoveItem(Item) -- finally, the item is removed from the original units inventory.
                 		end
 	        	end
 		end
 	end
+end
+
+function StoneStun(keys)
+
 end
