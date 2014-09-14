2a48195c2ad0caae9db512ff9464b7dffc48ed5d
dli42 <davelid136@gmail.com>
2014-09-14 18:08:19 -0400

    Added Mage Masher abilities

--- scripts/vscripts/custom_functions_item.lua
+++ scripts/vscripts/custom_functions_item.lua
@@ -62,3 +62,31 @@ function EatMeatRaw(keys)	--triggers the meat eating channel ability
     --caster:RemoveAbility(abilityName)
 	--]]
 end
+
+function MageMasherManaBurn(keys)
+	local caster = keys.caster
+    local target = keys.target
+	local damage = keys.Damage
+	local targetName = target:GetUnitName()
+	--look for mage and priests only
+    if ((string.find(targetName,"mage") ~= nil) or (string.find(targetName,"priest")~= nil)) then
+        --print("Burning " .. damage .. " mana")
+		local startingMana = target:GetMana()
+		target:SetMana(startingMana - damage)
+		--print("Old mana " .. startingMana .. ". New Mana " .. target:GetMana())
+		
+		local damageTable = {
+		victim = target,
+		attacker = caster,
+		damage = damage,
+		damage_type = DAMAGE_TYPE_MAGICAL}						
+	
+		ApplyDamage(damageTable)
+		
+		local thisParticle = ParticleManager:CreateParticle("particles/units/heroes/hero_nyx_assassin/nyx_assassin_mana_burn.vpcf", PATTACH_ABSORIGIN, target)
+		ParticleManager:ReleaseParticleIndex(thisParticle)
+		target:EmitSound("Hero_NyxAssassin.ManaBurn.Target")
+	else
+		print(targetName .. " is not Mage or Priest")
+    end	
+end
