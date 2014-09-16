46244067d75afc56ff78bb82ff04a7e37a01cc52
dli42 <davelid136@gmail.com>
2014-09-16 00:35:34 -0400

    Precaching poison particles

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -1,33 +1,33 @@
 --[[
-    The purpose of this file is mostly to exist because it is required.
-    Deal with all precaching here, to keep it out of the way.
+	The purpose of this file is mostly to exist because it is required.
+	Deal with all precaching here, to keep it out of the way.
 ]]--
 
 print("addon_game_mode invoked")
 
 --[[
-    We want to precache resources we want to use, so they are ready for loading when the engine requires
-    Precache things we know we'll use.  Possible file types include (but not limited to):
-        PrecacheResource( "model", "*.vmdl", context )
-        PrecacheResource( "soundfile", "*.vsndevts", context )
-        PrecacheResource( "particle", "*.vpcf", context )
-        PrecacheResource( "particle_folder", "particles/folder", context )
-    This is called before the game is activated, but after the "cruft" has run.
+	We want to precache resources we want to use, so they are ready for loading when the engine requires
+	Precache things we know we"ll use.  Possible file types include (but not limited to):
+		PrecacheResource( "model", "*.vmdl", context )
+		PrecacheResource( "soundfile", "*.vsndevts", context )
+		PrecacheResource( "particle", "*.vpcf", context )
+		PrecacheResource( "particle_folder", "particles/folder", context )
+	This is called before the game is activated, but after the "cruft" has run.
 ]]--
 function Precache( context )
-    print("Precache Begin")
-    
-    PrecacheItemByNameSync( "item_tinder", context )
-    PrecacheItemByNameSync( "item_raw_meat", context )
-    PrecacheItemByNameSync( "item_bone", context )
+	print("Precache Begin")
+	
+	PrecacheItemByNameSync( "item_tinder", context )
+	PrecacheItemByNameSync( "item_raw_meat", context )
+	PrecacheItemByNameSync( "item_bone", context )
 
-    PrecacheUnitByNameSync( "npc_dota_creature_elk", context )
-    PrecacheUnitByNameSync( "npc_dota_creature_hawk", context )
+	PrecacheUnitByNameSync( "npc_dota_creature_elk", context )
+	PrecacheUnitByNameSync( "npc_dota_creature_hawk", context )
 
-    PrecacheUnitByNameSync( "npc_dota_hero_huskar", context )
-    PrecacheUnitByNameSync( "npc_dota_hero_witch_doctor", context )
-    PrecacheUnitByNameSync( "npc_dota_hero_dazzle", context )
-    PrecacheUnitByNameSync( "npc_dota_hero_troll_warlord", context )
+	PrecacheUnitByNameSync( "npc_dota_hero_huskar", context )
+	PrecacheUnitByNameSync( "npc_dota_hero_witch_doctor", context )
+	PrecacheUnitByNameSync( "npc_dota_hero_dazzle", context )
+	PrecacheUnitByNameSync( "npc_dota_hero_troll_warlord", context )
 
 	PrecacheResource("model", "models/props_debris/camp_fire001.vmdl",context)
 	PrecacheResource("model", "models/props_structures/tent_dk_small.vmdl",context)
@@ -45,17 +45,20 @@ function Precache( context )
 	PrecacheResource("model", "models/items/wards/nexon_sotdaeward/nexon_sotdaeward.vmdl",context)
 	PrecacheResource("model", "models/items/furion/staff_eagle_1.vmdl",context)
 	PrecacheResource("particle","particles/dire_fx/fire_barracks_glow_b.vpcf",context)
-    
-    PrecacheResource("model", 'models/props_destruction/lion_groundspikes.vmdl',context)
-    PrecacheResource("model", 'models/items/abaddon/alliance_abba_weapon/alliance_abba_weapon_fx.vmdl',context)
-    PrecacheResource("model", 'models/particle/tiny_simrocks.vmdl',context)
-    PrecacheResource("model", 'models/particle/ice_shards.vmdl',context)
 	
-	PrecacheResource("particle_folder", 'particles/units/heroes/hero_nyx_assassin',context)
-    PrecacheResource("soundfile", 'soundevents/game_sounds_heroes/game_sounds_nyx_assassin.vsndevts',context)
-	PrecacheResource("soundfile", 'soundevents/game_sounds/ability_catapult_attack.vsndevts',context)
+	PrecacheResource("model", "models/props_destruction/lion_groundspikes.vmdl",context)
+	PrecacheResource("model", "models/items/abaddon/alliance_abba_weapon/alliance_abba_weapon_fx.vmdl",context)
+	PrecacheResource("model", "models/particle/tiny_simrocks.vmdl",context)
+	PrecacheResource("model", "models/particle/ice_shards.vmdl",context)
 	
-    print("Precache Finish")
+	PrecacheResource("particle_folder", "particles/units/heroes/hero_nyx_assassin",context)
+	PrecacheResource("soundfile", "soundevents/game_sounds_heroes/game_sounds_nyx_assassin.vsndevts",context)
+	PrecacheResource("soundfile", "soundevents/game_sounds/ability_catapult_attack.vsndevts",context)
+	PrecacheResource("particle_folder", "particles/units/heroes/hero_venomancer",context)
+	PrecacheResource("soundfile", "soundevents/game_sounds_heroes/game_sounds_venomancer.vsndevts",context)
+	
+				
+	print("Precache Finish")
 end
 
 
