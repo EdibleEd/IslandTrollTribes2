b84784390ff9fcd2638bfb49cfe305cd1aeb471a
dli42 <davelid136@gmail.com>
2014-10-02 11:22:42 -0400

    Fervor potion fixed, medallion of courage name changed

--- scripts/vscripts/custom_functions_item.lua
+++ scripts/vscripts/custom_functions_item.lua
@@ -221,9 +221,37 @@ function PotionFervorUse(keys)
             return
         	end
     		)
-    local stoneApplier = CreateItem("item_stone", caster, caster)
-    
+end
 
+function PotionFervorSecondary(keys)
+	local caster = keys.caster
+	local entangleDur = 5
+	local entangleDur = 7.5
+	local entangle = CreateItem( "item_scroll_entangling", caster, caster)
+	local thistles = CreateItem( "item_gun_blow_thistles", caster, caster)
+
+	local enemiesInRange = FindUnitsInRadius(
+        caster:GetTeam(),
+        caster:GetOrigin(),
+        nil, 300,
+        DOTA_UNIT_TARGET_TEAM_ENEMY,
+        DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_CREEP,
+        DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE + DOTA_UNIT_TARGET_FLAG_NO_INVIS, 
+        FIND_CLOSEST,
+        false)
+    
+    print(#enemiesInRange)
+        
+	if #enemiesInRange > 0 then
+		for i = 1, #enemiesInRange do
+			local randomNum = RandomInt(0, 1)
+			if randomNum == 0 then
+				entangle:ApplyDataDrivenModifier( caster, enemiesInRange[i], "modifier_scroll_entanglingroots", {duration=entangleDur})
+			else
+				thistles:ApplyDataDrivenModifier( caster, enemiesInRange[i], "modifier_gun_blow_thistles", {duration=thistleDur})
+			end
+		end
+	end
 end
 
 function CastPurge(keys)
