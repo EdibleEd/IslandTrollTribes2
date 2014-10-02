a0c69f2e68195044dd6eb65df2a646548f21333f
dli42 <davelid136@gmail.com>
2014-10-02 19:02:51 -0400

    Blowgun reload functionality, poison application (commented out)

--- scripts/vscripts/custom_functions_item.lua
+++ scripts/vscripts/custom_functions_item.lua
@@ -271,6 +271,80 @@ function CastPurge(keys)
     --dummy_caster:ForceKill(true)
 end
 
+function PotionPoisonUse(keys)
+	local caster = keys.caster
+	local oldItemName = keys.OldItemName
+	local newItemName = keys.NewItemName
+		for itemSlot = 0, 5, 1 do
+		if caster ~= nil then
+			local Item = killedUnit:GetItemInSlot( itemSlot )
+			if Item ~= nil and Item:GetName() == oldItemName then
+				local itemCharges = Item:GetCurrentCharges()
+				local newItem = CreateItem(newItemName, nil, nil) 
+				newItem:SetCurrentCharges(itemCharges)
+				caster:RemoveItem(Item)
+				caster:AddItem(itemName)
+				return
+			end
+		end
+	end
+end
+
+function ReloadItem(keys)
+	local caster = keys.caster
+	local ammoItem = keys.ability
+	local ammoCharges = ammoItem:GetCurrentCharges()
+	local maximumAmmo = keys.MaxStacks
+	if maximumAmmo == nil then
+		maximumAmmo = 3
+	end
+	if ammoCharges == 0 then
+		ammoCharges = 1 --to account for ammo like bone, with no charges
+	end
+	local emptyItem = keys.EmptyItem
+	local loadedItem = keys.LoadedItem
+
+	--looks for already loaded weapon of the same type first, then looks for empty
+	for itemSlot = 0, 5, 1 do
+		if caster ~= nil then
+			local Item = caster:GetItemInSlot( itemSlot )
+			if Item ~= nil and Item:GetName() == loadedItem then
+				local itemCharges = Item:GetCurrentCharges() + ammoCharges
+				if itemCharges > maximumAmmo then
+					ammoItem:SetCurrentCharges(itemCharges - maximumAmmo)
+					Item:SetCurrentCharges(maximumAmmo)
+				else
+					Item:SetCurrentCharges(itemCharges)
+					caster:RemoveItem(ammoItem)
+					return
+				end
+			end
+		end
+	end
+	for itemSlot = 0, 5, 1 do
+		if caster ~= nil then
+			local Item = caster:GetItemInSlot( itemSlot )
+			if Item~= nil and Item:GetName() == emptyItem then
+				local itemCharges = Item:GetCurrentCharges() + ammoCharges
+				local newItem = CreateItem(loadedItem, nil, nil)
+				if itemCharges > maximumAmmo then
+					ammoItem:SetCurrentCharges(itemCharges - maximumAmmo)
+					newItem:SetCurrentCharges(maximumAmmo)
+					caster:RemoveItem(Item)
+					caster:AddItem(newItem)
+				else
+					newItem:SetCurrentCharges(ammoCharges)
+					caster:RemoveItem(Item)
+					caster:AddItem(newItem)
+					caster:RemoveItem(ammoItem)
+					return
+				end
+
+			end
+		end
+	end
+end
+
 function SummonSkeleton(keys)
 	local caster = keys.caster
 
