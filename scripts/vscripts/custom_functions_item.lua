d610b102e7dcf97448796ed79bfe9e1b22486da7
dli42 <davelid136@gmail.com>
2014-10-02 22:37:50 -0400

    Made blowguns revert to empty upon being spent, fixed some related bugs
    Signed-off-by: dli42 <davelid136@gmail.com>

--- scripts/vscripts/custom_functions_item.lua
+++ scripts/vscripts/custom_functions_item.lua
@@ -345,6 +345,17 @@ function ReloadItem(keys)
 	end
 end
 
+function GunBlowCheckEmpty(keys)
+	local caster = keys.caster
+	local item = keys.ability
+
+	if item:GetCurrentCharges() <= 0 then
+		local emptyGun = CreateItem("item_gun_blow_empty", nil, nil)
+		caster:RemoveItem(item)
+		caster:AddItem(emptyGun)
+	end
+end
+
 function SummonSkeleton(keys)
 	local caster = keys.caster
 
