af54ee90249a1979feda4197d3a14b9a6805964d
Averse <jaapnanninga@hotmail.com>
2014-10-08 14:16:13 +0200

    Added Blur (Escape Artist). Needs work.

--- scripts/vscripts/custom_functions_ability.lua
+++ scripts/vscripts/custom_functions_ability.lua
@@ -419,9 +419,27 @@ function Teleport(keys)
 	caster:SetAbsOrigin(point)
 end
 
-function CreateSmokeStream(keys)
+function BlurCollisionCheck(keys)
 	local caster = keys.caster
+	local teamnumber = caster:GetTeamNumber()
+	local casterPosition = caster:GetAbsOrigin()
 	
+	local units = FindUnitsInRadius(teamnumber,
+									casterPosition,
+									nil,
+									100,
+									DOTA_UNIT_TARGET_TEAM_ENEMY,
+									DOTA_UNIT_TARGET_HERO,
+									DOTA_UNIT_TARGET_FLAG_NONE,
+									FIND_ANY_ORDER,
+									false)	
+	local count = 0
+	for _ in pairs(units) do 
+		count = count + 1 
+	end
+	if count > 0 then
+		caster:ForceKill(true)
+	end
 end
 
 --utility functions
