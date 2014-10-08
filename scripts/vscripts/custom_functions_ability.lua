55ac1f32efcc3257e9fda2e79673c6d7a8606112
Averse <jaapnanninga@hotmail.com>
2014-10-08 19:53:19 +0200

    Added Depress (Mage) mana loss + speed malus

--- scripts/vscripts/custom_functions_ability.lua
+++ scripts/vscripts/custom_functions_ability.lua
@@ -467,6 +467,18 @@ function RestoreMana(keys)
 	target:GiveMana(keys.ManaRestored) 
 end
 
+function RemoveMana(keys)
+	local target = keys.target
+	local manaloss = keys.ManaRemoved
+	
+    if target == nil then
+        target = keys.caster
+    end
+	
+	local mana = target:GetMana()
+	target:SetMana(mana-manaloss)
+end
+
 function ToggleAbility(keys)
 	local caster = keys.caster
 	
