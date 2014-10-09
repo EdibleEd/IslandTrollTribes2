82a15d5cf4cd891da77e2ed3227d6a44befb3b80
dli42 <davelid136@gmail.com>
2014-10-09 15:57:23 -0400

    Added recipe list

---
+++ scripts/vscripts/recipe_list.lua
@@ -0,0 +1,23 @@
+TROLL_RECIPE_TABLE = {
+        {"item_building_kit_fire_basic",        {"item_tinder", "item_flint", "item_stick"}},
+        {"item_building_kit_tent",              {"item_stick", "item_hide_any", "item_stick"}},
+        {"item_building_kit_troll_hut",         {"item_ball_clay", "item_stick", "item_stick", "item_hide_any", "item_hide_any"}},
+        {"item_building_kit_hut_mud",           {"item_ball_clay", "item_ball_clay", "item_ball_clay", "item_ball_clay"}},
+        {"item_building_kit_storage_chest",     {"item_hide_any", "item_stick", "item_tinder", "item_ball_clay"}},
+        {"item_building_kit_smoke_house",       {"item_stick", "item_hide_any", "item_ball_clay"}},
+        {"item_building_kit_armory",            {"item_flint", "item_stone", "item_stone", "item_stone"}},
+        {"item_building_kit_tannery",           {"item_stick", "item_stick", "item_stone", "item_stone"}},
+        {"item_building_kit_workshop",          {"item_stick", "item_ingot_iron", "item_ingot_iron"}},
+        {"item_building_kit_hut_witch_doctor",  {"item_stick", "item_crystal_mana", "item_stick", "item_crystal_mana","item_stick", "item_crystal_mana"}},
+        {"item_building_kit_mixing_pot",        {"item_stick", "item_ball_clay", "item_ball_clay", "item_ball_clay"}},
+        {"item_building_kit_tower_omni",        {"item_stone", "item_stick", "item_stick", "item_stick"}},
+        {"item_building_kit_teleport_beacon",   {"item_stone", "item_stone", "item_crystal_mana", "item_crystal_mana"}},
+        {"item_building_kit_hatchery",          {"item_stone", "item_stone", "item_stone", "item_stick", "item_stick", "item_stick"}}
+    }
+HIDE_ALIAS_TABLE = {
+        {"item_hide_any", {"item_hide_wolf", "item_hide_elk", "item_hide_jungle_bear"}}
+    }
+
+ARMORY_RECIPE_TABLE = {
+        {"item_ingot_iron",                     {"item_stone", "item_flint"}},
+    }
