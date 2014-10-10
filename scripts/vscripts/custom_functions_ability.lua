fe0a2f067cc3731937818527579a2ab22c6ca8ab
Averse <jaapnanninga@hotmail.com>
2014-10-10 15:48:43 +0200

    Fixed Reduce Food (Mage)

--- scripts/vscripts/custom_functions_ability.lua
+++ scripts/vscripts/custom_functions_ability.lua
@@ -416,20 +416,20 @@ end
 function ReduceFood(keys)
 	local target = keys.target
 	local reduction = RandomInt(0,2)
-	local i = 0
 	
-	while reduction ~= 0 do
+	for i=0,5 do
 		local item = target:GetItemInSlot(i)
 		if item ~= nil then
 			local itemName = target:GetItemInSlot(i):GetName()
 			if itemName == "item_meat_cooked" then
 				local charges = item:GetCurrentCharges()
-				if charges <= reduction then
-					target:RemoveItem(item)
-					reduction = reduction - charges
+				local newCharges = charges-reduction
+				if newCharges < 1 then
+					item:SetCurrentCharges(1)
 				else
-					item:SetCurrentCharges(charges - reduction)
+					item:SetCurrentCharges(newCharges)
 				end
+				break
 			end
 		end
 	end
