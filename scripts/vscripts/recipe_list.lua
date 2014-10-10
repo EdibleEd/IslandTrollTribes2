0ff88d2828898f6ec7be41748a54916860b15ff1
Vendivar <Vendivar@gmail.com>
2014-10-10 13:24:34 -0400

    Added the rest of the recipies!
    Added the rest of the recipies!

--- scripts/vscripts/recipe_list.lua
+++ scripts/vscripts/recipe_list.lua
@@ -15,9 +15,87 @@ TROLL_RECIPE_TABLE = {
         {"item_building_kit_hatchery",          {"item_stone", "item_stone", "item_stone", "item_stick", "item_stick", "item_stick"}}
     }
 HIDE_ALIAS_TABLE = {
-        {"item_hide_any", {"item_hide_wolf", "item_hide_elk", "item_hide_jungle_bear"}}
+        {"item_hide_any", 						{"item_hide_wolf", "item_hide_elk", "item_hide_jungle_bear"}}
     }
-
+ITEM_ALIAS_TABLE = {
+        {"item_stickorbone", 					{"item_stick", "item_bone"}}
+		{"item_gloves_basic", 					{"item_gloves_elk", "item_gloves_wolf", "item_gloves_bear"}}
+		{"item_boots_basic", 					{"item_boots_elk", "item_boots_wolf", "item_boots_bear"}}
+		{"item_coat_basic", 					{"item_coat_elk", "item_coat_wolf", "item_coat_bear"}}
+    }
 ARMORY_RECIPE_TABLE = {
-        {"item_ingot_iron",                     {"item_stone", "item_flint"}},
+        {"item_ingot_iron",                   	{"item_stone", "item_flint"}},
+		{"item_ingot_steel",               	    {"item_ingot_iron", "item_ingot_iron", "item_flint", "item_flint"}},
+		{"item_axe_flint",               	    {"item_stickorbone", "item_flint", "item_flint", }},
+		{"item_axe_stone",                	    {"item_stickorbone", "item_stone", "item_stone", }},
+		{"item_axe_iron",                	    {"item_stickorbone", "item_ingot_iron", "item_ingot_iron", }},
+		{"item_axe_steel",               	    {"item_stickorbone", "item_ingot_steel", "item_ingot_steel", }},
+		{"item_axe_item_axe_mage_masher",       {"item_stickorbone", "item_spirit_wind", "item_spirit_water", "item_crystal_mana"}},
+		{"item_spear_basic",       				{"item_stickorbone", "item_stone"}},
+		{"item_spear_iron",       				{"item_ingot_iron", "item_stickorbone"}},
+		{"item_spear_steel",       				{"item_ingot_steel", "item_stickorbone"}},
+		{"item_spear_dark",       				{"item_rock_dark", "item_stickorbone"}},
+		{"item_trap_ensnare",       			{"item_tinder", "item_stickorbone", "item_stick"}},	
+		{"item_boots_bone",       				{"item_boots_basic", "item_bone", "item_bone", "item_bone", "item_bone", "item_bone"}},		
+		{"item_gloves_bone",       				{"item_gloves_basic", "item_bone", "item_bone", "item_bone", "item_bone", "item_bone"}},	
+		{"item_coat_bone",       				{"item_coat_basic", "item_bone", "item_bone", "item_bone", "item_bone", "item_bone"}},
+		{"item_boots_iron",       				{"item_boots_basic", "item_ingot_iron", "item_ingot_iron"}},
+		{"item_gloves_iron",       				{"item_gloves_basic", "item_ingot_iron", "item_ingot_iron"}},
+		{"item_coat_iron",       				{"item_coat_basic", "item_ingot_iron", "item_ingot_iron"}},
+		{"item_boots_steel",       				{"item_boots_basic", "item_ingot_steel", "item_ingot_steel"}},
+		{"item_gloves_steel",       			{"item_gloves_basic", "item_ingot_steel", "item_ingot_steel"}},
+		{"item_coat_steel",       				{"item_coat_basic", "item_ingot_steel", "item_ingot_steel"}},
+		{"item_shield_basic",       			{"item_stick", "item_hide_any", "item_ball_clay", "item_ball_clay"}},
+		{"item_shield_bone",       				{"item_shield_basic", "item_bone", "item_bone", "item_bone", "item_bone", "item_bone"}},
+		{"item_shield_iron",       				{"item_shield_basic", "item_ingot_iron", "item_ingot_iron"}},
+		{"item_shield_steel",       			{"item_shield_basic", "item_ingot_steel", "item_ingot_steel"}},
     }
+WORKSHOP_RECIPE_TABLE = {
+		{"item_compass_sonar",       			{"item_ingot_iron", "item_flint"}},
+		{"item_blowgun_empty",       			{"item_stick", "item_ball_clay"}},
+		{"item_net_basic",       				{"item_tinder", "item_tinder"}},
+		{"item_bomb_smoke",       				{"item_acorn", "item_spirit_wind"}},
+		{"item_beehive",       					{"item_stick", "item_stick", "item_spirit_wind"}},
+		{"item_thistles_dark",       			{"item_thistles", "item_rock_dark"}},
+		{"item_armor_battle",       			{"item_coat_bone", "item_crystal_mana", "item_spirit_wind", "item_spirit_water"}},		
+		{"item_ship_transport",       			{"item_stick", "item_stick", "item_ball_clay", "item_ball_clay", "item_hide_any", "item_hide_any"}},
+		{"item_boots_anabolic",       			{"item_boots_bone", "item_spirit_wind", "item_spirit_water", "item_potion_anabolic", "item_potion_anabolic"}},
+		{"item_gloves_battle",       			{"item_gloves_bone", "item_ingot_steel", "item_ingot_iron", "item_crystal_mana", "item_spirit_wind", "item_spirit_wind"}},
+		{"item_weapon_emp",       				{"item_spirit_water", "item_spirit_wind", "item_crystal_mana"}},		
+		{"item_shield_battle",       			{"item_shield_bone", "item_hide_elk", "item_ingot_steel", "item_spirit_wind", "item_spirit_water", "item_crystal_mana"}},		
+		{"item_axe_battle",       				{"item_stick", "item_ingot_steel", "item_spirit_wind", "item_spirit_water", "item_hide_elk"}},	
+    }
+	
+	TANNERY_RECIPE_TABLE = {
+		{"item_gloves_elk",       				{"item_hide_elk", "item_hide_elk"}},
+		{"item_boots_elk",       				{"item_hide_elk", "item_hide_elk"}},
+		{"item_coat_elk",       				{"item_hide_elk", "item_hide_elk"}},
+		{"item_gloves_wolf",       				{"item_hide_wolf", "item_hide_wolf"}},
+		{"item_boots_wolf",       				{"item_hide_wolf", "item_hide_wolf"}},
+		{"item_coat_wolf",       				{"item_hide_wolf", "item_hide_wolf"}},
+		{"item_boots_bear",       				{"item_hide_bear", "item_hide_bear"}},
+		{"item_gloves_bear",       				{"item_hide_bear", "item_hide_bear"}},
+		{"item_coat_bear",       				{"item_hide_bear", "item_hide_bear"}},
+    }
+	
+	WDHUT_RECIPE_TABLE = {
+		{"item_scroll_fireball",       			{"item_flint", "item_crystal_mana"}},
+		{"item_scroll_entangling",       		{"item_tinder", "item_crystal_mana"}},
+		{"item_scroll_living_dead",       		{"item_bone", "item_crystal_mana"}},
+		{"item_scroll_stoneskin",       		{"item_stone", "item_crystal_mana"}},
+		{"item_scroll_tsunami",       			{"item_spirit_water", "item_crystal_mana"}},
+		{"item_scroll_cyclone",       			{"item_spirit_wind", "item_crystal_mana"}},
+		{"item_building_kit_fire_mage",       	{"item_building_kit_fire_basic", "item_crystal_mana"}},
+		{"item_building_kit_spirit_ward",       {"item_crystal_mana", "item_stick", "item_crystal_mana", "item_crystal_mana"}},
+		{"item_seed_magic",       				{"item_stick", "item_crystal_mana"}},
+		{"item_poison",       					{"item_mushroom", "item_mushroom", "item_mushroom"}},
+		{"item_poison_ultra",       			{"item_poison", "item_poison"}},
+		{"item_clay_living",       				{"item_ball_clay", "item_crystal_mana"}},
+		{"item_spear_poison",       			{"item_spear_basic", "item_mushroom"}},
+		{"item_spear_poison_refined",       	{"item_spear_basic", "item_poison"}},
+		{"item_spear_poison_ultra",       		{"item_spear_basic", "item_poison_ultra"}},
+		{"item_cloak_protection",       		{"item_magic_raw", "item_coat_bone", "item_herb_butsu"}},
+		{"item_cloak_frost",       				{"item_magic_raw", "item_coat_bone", "item_spirit_water"}},
+		{"item_cloak_fire",       				{"item_magic_raw", "item_coat_bone", "item_flint"}},
+    }
+	
