75b0d14c80767da55de869e0d97913b057e47a39
dli42 <davelid136@gmail.com>
2014-10-14 14:07:32 -0400

    Added variables to account for different neutrals

--- scripts/vscripts/passive_neutral_ai.lua
+++ scripts/vscripts/passive_neutral_ai.lua
@@ -1,10 +1,26 @@
 function Spawn(entityKeyValues)
-	thisEntity:SetContextThink("passive_neutral_ai_think"..thisEntity:GetEntityIndex(), PassiveNeutralThink, 0.25)
+	thisEntity:SetContextThink("passive_neutral_ai_think"..thisEntity:GetEntityIndex(), PassiveNeutralThink, 0.5)
 	thisEntity.state = "wander"		--possible states = wander, flee
-
-	print("starting passive neutral ai")
+	if string.find(thisEntity:GetUnitName(), "elk") then
+		thisEntity.WanderDistance = 1000
+		thisEntity.FleeDistance = 2000
+		thisEntity.MinWaitTime = 5
+		thisEntity.MaxWaitTime = 20
+	elseif string.find(thisEntity:GetUnitName(), "fish") then
+		thisEntity.WanderDistance = 300
+		thisEntity.FleeDistance = 300
+		thisEntity.MinWaitTime = 15
+		thisEntity.MaxWaitTime = 30
+	elseif string.find(thisEntity:GetUnitName(), "hawk") then
+		thisEntity.WanderDistance = 3000
+		thisEntity.FleeDistance = 3000
+		thisEntity.MinWaitTime = 3
+		thisEntity.MaxWaitTime = 7.5
+	end
+	print("starting passive neutral ai for "..thisEntity:GetUnitName()..thisEntity:GetEntityIndex())
 
 	thisEntity.spawnTime = GameRules:GetGameTime()
+	thisEntity.wander_wait_time = GameRules:GetGameTime() + 0	
 end
 
 function PassiveNeutralThink()
@@ -13,12 +29,21 @@ function PassiveNeutralThink()
 	end
 
 	if (thisEntity.state == "wander") then
-		local newPosition = thisEntity:GetAbsOrigin() + RandomVector(1000)
+		if GameRules:GetGameTime() >= thisEntity.wander_wait_time then
+			local newPosition = thisEntity:GetAbsOrigin() + RandomVector(thisEntity.WanderDistance)
+			while newPosition.x > 8000 or newPosition.x < -8000 or newPosition.y > 8000 or newPosition.y < -8000 do
+				newPosition = thisEntity:GetAbsOrigin() + RandomVector(thisEntity.WanderDistance)
+			end
+			thisEntity:MoveToPosition(newPosition)
+
+			thisEntity.wander_wait_time = GameRules:GetGameTime() + RandomFloat(thisEntity.MinWaitTime, thisEntity.MaxWaitTime)
+		end
+	elseif (thisEntity.state == "flee") then
+		local newPosition = thisEntity:GetAbsOrigin() + RandomVector(thisEntity.FleeDistance)
 		thisEntity:MoveToPosition(newPosition)
-		
-    elseif (thisEntity.state == "flee") then
-    	
+		thisEntity.wander_wait_time = GameRules:GetGameTime() + RandomFloat(thisEntity.MinWaitTime, thisEntity.MaxWaitTime)
+		thisEntity.state = "wander"
 	end
 
-	return RandomFloat(10, 30)
+	return 0.5
 end
