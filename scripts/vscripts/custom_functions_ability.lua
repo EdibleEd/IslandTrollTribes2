a470b9ce1668d45ba4c7400a8a9c154898677bdd
Averse <jaapnanninga@hotmail.com>
2014-11-11 12:40:49 +0100

    Thief can no longer Teleport to areas in fow

--- scripts/vscripts/custom_functions_ability.lua
+++ scripts/vscripts/custom_functions_ability.lua
@@ -823,7 +823,18 @@ end
 function Teleport(keys)
 	local caster = keys.caster
 	local point = keys.target_points[1]
-	caster:SetAbsOrigin(point)
+	
+	local dummyTarget = CreateUnitByName("dummy_caster_metronome", point, false, nil, nil,DOTA_TEAM_NEUTRALS)
+	local visible = caster:CanEntityBeSeenByMyTeam(dummyTarget)
+
+	if visible then
+		caster:SetAbsOrigin(point)
+	else
+		local tp = caster:FindAbilityByName("ability_thief_teleport") 
+		tp:EndCooldown()
+		local mana = tp:GetManaCost(1)
+		caster:GiveMana(mana)
+	end
 end
 
 function BlurCollisionCheck(keys)
